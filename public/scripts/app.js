305ccf637b04816f396fb42516733dd3d3bad941
QilinGu <gump423@gmail.com>
2016-08-27 10:13:57 -0700

    Add login and signup, but not completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,14 @@ app.config(function ($routeProvider) {
 	    	templateUrl: "./public/views/home.html",
 	    	controller: "homeController"
 	    })
+	    .when('/login', {
+	    	templateUrl: './public/views/login.html',
+	    	controller: 'loginController'
+	    })
+	    .when('/signup', {
+	    	templateUrl: './public/views/signup.html',
+	    	controller: 'signupController'
+	    })
 	    .when("/urls/:shortUrl",
 	    {
 	    	templateUrl: "./public/views/url.html",
---
+++ public/scripts/controllers/signupController.js
@@ -0,0 +1,11 @@
+var app = angular.module("tinyurlApp");
+
+angular.module('MyApp')
+  .controller('LoginCtrl', ['$scope', 'Auth', function($scope, Auth) {
+    $scope.login = function() {
+      Auth.login({
+        email: $scope.email,
+        password: $scope.password
+      });
+    };
+  }]);
